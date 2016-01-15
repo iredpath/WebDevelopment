f188b1764c3f42a24efbeec9844474a9395be937
Ian Redpath <iredpath@ccs.neu.edu>
2016-01-15 17:15:44 -0500

    script to help branch merges, github code pushes

---
+++ merge-branch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+BRANCH_NAME=$1
+
+if [ -z $BRANCH_NAME ]; then
+	echo "Usage: merge-branch branch-name"
+	exit 1
+fi
+
+# checkout master
+git checkout master
+if [ $? -ne 0 ]; then
+	echo "ERROR: unable to checkout branch master"
+	exit 1
+fi
+
+# merge in our branch
+git merge $BRANCH_NAME
+if [ $? -ne 0 ]; then
+	echo "ERROR: unable to merge branch $BRANCH_NAME with master"
+	exit 1
+fi
+
+# push changes to openshift
+git push -u openshift master
+if [ $? -ne 0 ]; then
+	echo "ERROR: unable to push changes to openshift remote"
+	exit 1
+fi
+
+# deploy master
+rhc deploy master -a WebDevelopment
+if [ $? -ne 0 ]; then
+	echo "ERROR: unable to deploy branch master" # should never happen, this script is only run after successful deploy
+	exit 1
+fi
+
+# push to github
+git push -u github master
+if [ $? -ne 0 ]; then
+	echo "ERROR: unable to push changes to github remote"
+	exit 1
+fi

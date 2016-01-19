4f58ac4ddd0842884fc5b1e84b33d104b42c5928
Ian Redpath <iredpath@ccs.neu.edu>
2016-01-19 10:11:45 -0500

    info messages in deploy-branch script

--- merge-branch.sh
+++ merge-branch.sh
@@ -19,6 +19,8 @@ git merge $BRANCH_NAME
 if [ $? -ne 0 ]; then
 	echo "ERROR: unable to merge branch $BRANCH_NAME with master"
 	exit 1
+else:
+	echo "INFO: branch ${BRANCH_NAME} successfully merged into openshift master"
 fi
 
 # push changes to openshift
@@ -26,18 +28,22 @@ git push -u openshift master
 if [ $? -ne 0 ]; then
 	echo "ERROR: unable to push changes to openshift remote"
 	exit 1
-fi
+else:
+	echo "INFO: local changes successfully pushed to openshift master"
 
 # deploy master
 rhc deploy master -a WebDevelopment
 if [ $? -ne 0 ]; then
 	echo "ERROR: unable to deploy branch master" # should never happen, this script is only run after successful deploy
 	exit 1
-fi
+else:
+	echo "INFO: openshift master branch successfully delpoyed"
 
 # push to github
 git push -u github master
 if [ $? -ne 0 ]; then
 	echo "ERROR: unable to push changes to github remote"
 	exit 1
+else:
+	echo "INFO: openshift master branch successfully pushed to github"
 fi

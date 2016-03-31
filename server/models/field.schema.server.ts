0361f38127b892d90ba65fff7212c1cf1e93f649
Ian Redpath <iredpath@ccs.neu.edu>
2016-03-31 16:00:04 -0400

    readd textarea to enum

--- server/models/field.schema.server.ts
+++ server/models/field.schema.server.ts
@@ -4,7 +4,7 @@ export default function() {
 
 	const FieldSchema = new mongoose.Schema({
 		label: String,
-		type: { type: String, enum: ['TEXT', 'EMAIL', 'PASSWORD', 'RADIOS', 'CHECKBOXES', 'OPTIONS', 'DATE'] },
+		type: { type: String, enum: ['TEXT', 'TEXTAREA', 'EMAIL', 'PASSWORD', 'RADIOS', 'CHECKBOXES', 'OPTIONS', 'DATE'] },
 		placeholder: String,
 		options: [{ label: String, value: String }]
 	}, { collection: 'assignment.field' })

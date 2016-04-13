550d11cf47f8ad24975cf87fa356100e4c55c228
Ian Redpath <iredpath@ccs.neu.edu>
2016-04-13 12:46:57 -0400

    session preservation changes

--- public/project/components/home/home.ts
+++ public/project/components/home/home.ts
@@ -1,4 +1,5 @@
 import { View, Component } from 'angular2/core'
+import { UserService } from '../../services/userService'
 
 @Component({
 	selector: "vml-home"
@@ -8,5 +9,8 @@ import { View, Component } from 'angular2/core'
 })
 
 export class Home {
-
+	constructor(public userService: UserService) {
+		userService.loggedIn()
+			.subscribe(resp => { resp.json() && resp.json().user && this.userService.setActiveUser(resp.json().user) })
+	}
 }

37e1a39ea7d8f315300a22ef1803fe9cb97e66e0
Ian Redpath <iredpath@ccs.neu.edu>
2016-04-12 22:37:42 -0400

    add typescript dependencies

--- server/typings/tsd.d.ts
+++ server/typings/tsd.d.ts
@@ -3,3 +3,7 @@
 /// <reference path="mime/mime.d.ts" />
 /// <reference path="node/node.d.ts" />
 /// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="express-serve-static-core/express-serve-static-core.d.ts" />
+/// <reference path="express-session/express-session.d.ts" />
+/// <reference path="passport-local/passport-local.d.ts" />
+/// <reference path="passport/passport.d.ts" />
